5ea21573fb9c6e51b14525e8b962063de5f0265b
shjchen <zdbd@vip.qq.com>
2018-02-26 23:49:28 +0800

    add ItemRepositoryTest file.

---
+++ tests/ItemRepositoryTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/26
+ * Time: 23:48
+ */
+
+namespace iBrand\Component\Groupon\Test;
+
+
+class ItemRepositoryTest extends BaseTest
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
